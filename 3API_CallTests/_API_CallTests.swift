34385406b6d775a04620e0b63eb0c60cac820fbc
Anusha Raju <anusha.raju0070@gmail.com>
2024-12-16 07:05:23 -0800

    Initial Commit

---
+++ 3API_CallTests/_API_CallTests.swift
@@ -0,0 +1,17 @@
+//
+//  _API_CallTests.swift
+//  3API_CallTests
+//
+//  Created by Anusha Raju on 12/16/24.
+//
+
+import Testing
+@testable import _API_Call
+
+struct _API_CallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
